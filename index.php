d7efb1432bfb9e7f9b57d6cd7fc4577d34b70b40
Agustin <joseagustindelosreye@gmail.com>
2022-11-20 16:24:56 -0300

    pelea con el grid de productos

--- index.php
+++ index.php
@@ -18,6 +18,8 @@
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
     <link rel="stylesheet" href="admin/css/stripe.css">
+    <!-- Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 <?php
     session_start();
     $accion=$_REQUEST['accion']??'';
@@ -35,7 +37,7 @@
 include_once "admin/db_ecommerce.php";
 $con = mysqli_connect($host, $user, $pass, $db);
 ?>
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
             <div class="col-12">                
                 <?php
@@ -82,8 +84,8 @@ $con = mysqli_connect($host, $user, $pass, $db);
 
     <!-- jQuery UI 1.11.4 -->
     <script src="admin/plugins/jquery-ui/jquery-ui.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Bootstrap 5 -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <!-- daterangepicker -->
     <script src="admin/plugins/moment/moment.min.js"></script>
     <script src="admin/plugins/daterangepicker/daterangepicker.js"></script>
